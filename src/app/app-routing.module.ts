947c9d1c9b3eca6149eee13a3d085f83f2c01c47
Prathin <pratinrp@gmail.com>
2025-03-24 14:32:44 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { LoginComponent } from './components/login/login.component';
+import { EnquiriesComponent } from './components/enquiries/enquiries.component';
+import { AdminEnquiriesComponent } from './components/admin-enquiries/admin-enquiries.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'enquiries',
+    component: EnquiriesComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'admin/enquiries',
+    component: AdminEnquiriesComponent,
+    canActivate: [AuthGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
